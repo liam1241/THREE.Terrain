46029eab2b7868d87f5eff25d6156f26e943469f
Liam Baron <liamb@hey.com>
2022-03-17 00:54:45 -0500

    Replaced Math.random() with Alea prng for Fault generation

--- src/generators.js
+++ src/generators.js
@@ -187,13 +187,15 @@ THREE.Terrain.Fault = function(g, options) {
     var d = Math.sqrt(options.xSegments*options.xSegments + options.ySegments*options.ySegments),
         iterations = d * options.frequency,
         range = (options.maxHeight - options.minHeight) * 0.5,
+        ranSeed = options.suppliedSeed,
         displacement = range / iterations,
         smoothDistance = Math.min(options.xSize / options.xSegments, options.ySize / options.ySegments) * options.frequency;
+    var prng = new Alea(ranSeed);
     for (var k = 0; k < iterations; k++) {
-        var v = Math.random(),
+        var v = prng(),
             a = Math.sin(v * Math.PI * 2),
             b = Math.cos(v * Math.PI * 2),
-            c = Math.random() * d - d*0.5;
+            c = prng() * d - d*0.5;
         for (var i = 0, xl = options.xSegments + 1; i < xl; i++) {
             for (var j = 0, yl = options.ySegments + 1; j < yl; j++) {
                 var distance = a*i + b*j - c;
